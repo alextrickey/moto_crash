4b30d8e571fc011958e6ede280035ae28ebd73c2
Alex Trickey <k.alex.trickey@gmail.com>
2017-03-15 21:06:30 -0700

    data: Made minor modifications to preserved variables

--- preprocess.r
+++ preprocess.r
@@ -3,10 +3,9 @@ setwd('~/Desktop/moto_crash')
 
 require(dplyr)
 require(missForest)
-#require(ggmap)
 source('SWITRS_func_and_lookups.r')
 
-#SWITRS Collision Data
+#SWITRS -- Collision Data
 #http://iswitrs.chp.ca.gov/Reports/jsp/userLogin.jsp
 #   Data Requested: 
 #     Bike, Pedestrian and Motorcycle Data
@@ -17,22 +16,19 @@ collision = tbl_df(read.csv('~/Desktop/moto_crash/switrs/CollisionRecords.txt',
                      strip.white = TRUE, 
                      na.strings = c("NA","","-"))) 
 
-# ## Additional data sources not currently in use
-# ##    (Further work needed to clean/link the datasets)
-#
-# #SWITRS Party Table - (Contains records for victims as well)
+# #SWITRS -- Collision Party Data 
 # party = tbl_df(read.csv('~/Desktop/moto_crash/switrs/PartyRecords.txt',
 #                             header=T, 
 #                             strip.white = T, 
 #                             na.strings = c("NA","","-"))) 
 # 
-# #SWITRS Victim Table 
+# #SWITRS -- Collision Victim Data 
 # victim = tbl_df(read.csv('~/Desktop/moto_crash/switrs/VictimRecords.txt',
 #                         header=T, 
 #                         strip.white = T, 
 #                         na.strings = c("NA","","-"))) 
 #
-# # Road Quality Data
+# #LA Public Works -- Road Surface Condition
 # #https://data.lacity.org/A-Livable-and-Sustainable-City/Road-Surface-Condition-Map/d9rz-k88a
 # pci = tbl_df(read.csv('~/Desktop/moto_crash/road_conditions_2017.csv',
 #                stringsAsFactors = F,
@@ -42,13 +38,23 @@ collision = tbl_df(read.csv('~/Desktop/moto_crash/switrs/CollisionRecords.txt',
 #Select Relevant Data
 moto_dat = collision %>%
   filter(MOTORCYCLE_ACCIDENT == 'Y') %>%
-  select(CASE_ID, COUNT_MC_KILLED, COUNT_MC_INJURED, COLLISION_SEVERITY,
-         COLLISION_DATE, ACCIDENT_YEAR, COLLISION_TIME, DAY_OF_WEEK,
-         INTERSECTION, WEATHER_1, WEATHER_2, STATE_HWY_IND, 
-         PCF_VIOL_CATEGORY, TYPE_OF_COLLISION, MVIW, ROAD_SURFACE,
-         ROAD_COND_1, ROAD_COND_2, LIGHTING, CONTROL_DEVICE, #exclude?
-         ALCOHOL_INVOLVED, TRUCK_ACCIDENT, PRIMARY_RD, SECONDARY_RD)
-         #Add later? -- CHP_VEHTYPE_AT_FAULT / STWD_VEHTYPE_AT_FAULT
+  select(
+         #unique id for collision
+         CASE_ID, 
+         #outcomes
+         COUNT_MC_KILLED, COUNT_MC_INJURED, COLLISION_SEVERITY, 
+         #when
+         COLLISION_DATE, ACCIDENT_YEAR, COLLISION_TIME, DAY_OF_WEEK, 
+         #where
+         INTERSECTION, STATE_HWY_IND, PRIMARY_RD, SECONDARY_RD,
+         #environment/conditions
+         WEATHER_1, WEATHER_2, ROAD_SURFACE, ROAD_COND_1, ROAD_COND_2, LIGHTING,
+         #accident/collision details
+         PCF_VIOL_CATEGORY, HIT_AND_RUN, TYPE_OF_COLLISION, MVIW, 
+         ALCOHOL_INVOLVED, TRUCK_ACCIDENT, 
+         #covariates (impact collision-level severity measure)
+         PEDESTRIAN_ACCIDENT, BICYCLE_ACCIDENT
+         )
 
 #Look at Primary Collision Factor (PCF) Categories
 moto_dat %>% 
@@ -76,26 +82,33 @@ moto_dat =
                          'Y','N'))
   ) %>%
   #Calculate Some Time-Related Variables
-  mutate(rush_hour_am = as.factor(ifelse(
-              (COLLISION_TIME >= 700 & COLLISION_TIME <= 900), #7-9am
-              'Y','N')),
-         rush_hour_pm = as.factor(ifelse(
-              (COLLISION_TIME >= 1500 & COLLISION_TIME <= 1900), #3-7pm
-              'Y','N')),
+  mutate(accident_year = ACCIDENT_YEAR,
+         day_of_week = as.factor(day_lookup[DAY_OF_WEEK]),
+         rush_hour = as.factor(
+              #check for morning rush hour
+              ifelse(
+                  (COLLISION_TIME >= 700 & COLLISION_TIME <= 900 #7-9am
+                  & day_of_week %in% c('M','Tu','W','Th','F')
+                  ), 'AM',
+              #check for evening rush hour
+              ifelse(
+                  (COLLISION_TIME >= 1500 & COLLISION_TIME <= 1900 #3-7pm
+                  & day_of_week %in% c('M','Tu','W','Th','F')
+                  ), 'PM','N'))
+              ),
          late_night = as.factor(ifelse(
               (COLLISION_TIME >= 2300 | COLLISION_TIME <= 500), #11pm-5am
               'Y','N')),
-         day_of_week = as.factor(day_lookup[DAY_OF_WEEK])
+         hours_since_midnight = floor(COLLISION_TIME/100) #hours
+                                + (COLLISION_TIME %% 100)/60 #minutes (decimal)
   ) %>%
-  #Transform/Recode Weather Columns
+  #Transform/Recode Weather Columns [not mutually exclusive]
   mutate(w_clear  = as.factor(merge_and_recode(WEATHER_1,WEATHER_2,'A')),
          w_cloudy = as.factor(merge_and_recode(WEATHER_1,WEATHER_2,'B')),
-         w_rainy  = as.factor(merge_and_recode(WEATHER_1,WEATHER_2,'C'))
-         #These are too infrequent... exclude
-         #w_snowy  = as.factor(merge_and_recode(WEATHER_1,WEATHER_2,'D')),
-         #w_foggy  = as.factor(merge_and_recode(WEATHER_1,WEATHER_2,'E')),
-         #w_other  = as.factor(merge_and_recode(WEATHER_1,WEATHER_2,'F')), 
-         #w_windy  = as.factor(merge_and_recode(WEATHER_1,WEATHER_2,'G'))
+         w_rainy  = as.factor(merge_and_recode(WEATHER_1,WEATHER_2,'C')),
+         #Merge infrequent weather types [snowy, foggy, other, windy]
+         w_other  = as.factor(merge_and_recode(
+                      WEATHER_1,WEATHER_2,codes =c('D','E','F','G')))
   ) %>%
   #Transform Road, Highway Data
   mutate(
@@ -127,7 +140,9 @@ moto_dat =
     type_of_collision = as.factor(collision_type_lookup[TYPE_OF_COLLISION]),
     mviw = as.factor(mviw_lookup[MVIW]), #motor vehicle involved with
     alcohol_involved = as.factor(na_as_no(ALCOHOL_INVOLVED)),
-    truck_accident = as.factor(na_as_no(TRUCK_ACCIDENT))
+    truck_accident = as.factor(na_as_no(TRUCK_ACCIDENT)),
+    pedestrian_accident = as.factor(na_as_no(PEDESTRIAN_ACCIDENT)), 
+    bicycle_accident = as.factor(na_as_no(BICYCLE_ACCIDENT))
   ) %>%
   #Convert primary and secondary roads to cross streets 
   #(later lookup geocodes with: geocode(moto_dat$cross_street)
@@ -135,17 +150,23 @@ moto_dat =
     cross_street = paste(PRIMARY_RD,'and',SECONDARY_RD,'Los Angeles')
   ) %>%
   select(
-    #Outcomes of Interest
+    #unique id for collision
+    CASE_ID, 
+    #outcomes
     mc_killed, mc_injured, collision_severity, severe,
-    #When:
-    ACCIDENT_YEAR, season, rush_hour_am, rush_hour_pm, late_night, day_of_week,
-    #Environment Conditions:
+    #when 
+    accident_year, hours_since_midnight,
+    season, rush_hour, late_night, day_of_week,
+    #where 
+    intersection, state_hwy_ind, #cross_streets,
+    #environment/conditions
     starts_with('w_'), #weather variables
     road_not_dry, road_condition_issue, lighting, 
-    #Collision Details:
-    intersection, state_hwy_ind, 
+    #accident/collision details
     type_of_collision, pcf_viol_category, mviw, alcohol_involved, 
-    truck_accident
+    truck_accident, 
+    #covariates (impact collision-level severity measure)
+    pedestrian_accident, bicycle_accident
   )
 
 #View(moto_dat)
@@ -169,18 +190,21 @@ collision %>%
 
 #Use Random Forests to Impute Missing Values
 set.seed(1047)
-moto_dat.imp = missForest(as.data.frame(moto_dat),variablewise = TRUE)
+moto_dat.imp = missForest(as.data.frame(moto_dat[,-1]),variablewise = TRUE)
 
 #Out of Bag Error for Imputed Columns
-data.frame(names(moto_dat),moto_dat.imp$OOBerror)
+data.frame('total_NAs' = apply(sapply(moto_dat[,-1],is.na),2,sum),
+           'OOB_Error'=moto_dat.imp$OOBerror) #MSE/PFC
 
 #Save Dataset Variations
 write.csv(moto_dat,
           file = "processed_data/moto_dat_with_NA.csv",
           row.names = FALSE)
-write.csv(moto_dat.imp$ximp,
+write.csv(cbind(moto_dat[,1],moto_dat.imp$ximp),
           file = "processed_data/moto_dat_imputed_NA.csv",
           row.names = FALSE)
 write.csv(na.omit(moto_dat),
           file = "processed_data/moto_dat_dropped_NA.csv",
           row.names = FALSE)
+
+
