8350f0d260add79003935cf7c0ac5e4808a136ca
Alex Trickey <k.alex.trickey@gmail.com>
2019-11-16 10:17:15 +0000

    style: Partial linting

--- analyze.r
+++ analyze.r
@@ -1,7 +1,7 @@
 
 #Load and Start H2O
 require(h2o)
-h2o.init(nthreads=-1, max_mem_size = "8G")
+h2o.init(nthreads = -1, max_mem_size = "8G")
 
 ########################
 ## Load and Prep Data ##
@@ -9,7 +9,7 @@ h2o.init(nthreads=-1, max_mem_size = "8G")
 
 #Load Data into H2O Frame
 moto_dat <- h2o.importFile("processed_data/moto_dat_imputed_NA.csv",
-                           destination_frame = 'moto_dat')
+                           destination_frame = "moto_dat")
 #Alternative: "processed_data/moto_dat_dropped_NA.csv"
 
 #Split Samples
@@ -23,11 +23,11 @@ valid <- splits[[2]] #15%
 test  <- splits[[3]] #15%
 
 #Identify Features
-outcomes = c("mc_killed","mc_injured","collision_severity","severe")
-features_glm = setdiff(names(moto_dat),
-                       c('CASE_ID','hours_since_midnight',outcomes))
-features = setdiff(names(moto_dat),
-                       c('CASE_ID','rush_hour','late_night',outcomes))
+outcomes <- c("mc_killed", "mc_injured", "collision_severity", "severe")
+features_glm <- setdiff(names(moto_dat),
+                       c("CASE_ID", "hours_since_midnight", outcomes))
+features <- setdiff(names(moto_dat),
+                       c("CASE_ID", "rush_hour", "late_night", outcomes))
 
 
 #######################
@@ -42,7 +42,7 @@ glm_baseline <- h2o.glm(x = features_glm,
                    nfolds = 5,
                    lambda_search = TRUE,
                    family = "binomial")
-glm_perf <- h2o.performance(model = glm_baseline,newdata = valid)
+glm_perf <- h2o.performance(model = glm_baseline, newdata = valid)
 glm_perf
 #Mean Per-Class Error:  0.3366404
 #AUC:  0.7294094
@@ -50,11 +50,11 @@ glm_perf
 
 #Default Random Forest
 rf_baseline <- h2o.randomForest(x = features,
-                            y = 'severe',
+                            y = "severe",
                             training_frame = train,
                             model_id = "rf_baseline",
                             seed = 1067)
-rf_perf1 <- h2o.performance(model = rf_baseline,newdata = valid)
+rf_perf1 <- h2o.performance(model = rf_baseline, newdata = valid)
 rf_perf1
 #Mean Per-Class Error:  0.3179449
 #AUC:  0.7137967
@@ -66,7 +66,7 @@ gbm_baseline <- h2o.gbm(x = features,
                    training_frame = train,
                    model_id = "gbm_baseline",
                    seed = 1067)
-gbm_perf1 <- h2o.performance(model = gbm_baseline,newdata = valid)
+gbm_perf1 <- h2o.performance(model = gbm_baseline, newdata = valid)
 gbm_perf1
 #Mean Per-Class Error:  0.3106437
 #AUC:  0.7334899
@@ -78,12 +78,10 @@ gbm_perf1
 
 drf_params <- list(max_depth = seq(5, 15, 1),
                    min_rows = 1:10,
-                   ntrees = seq(10,150,10),
+                   ntrees = seq(10, 150, 10),
                    balance_classes = TRUE,
                    class_sampling_factors = list(
-                     c(1,1),
-                     #c(0.4,1),c(0.6,1),c(0.8,1),#under sample
-                     c(1,1.2),c(1,1.4),c(1,1.6)#over sample
+                     c(1, 1), c(1, 1.2), c(1, 1.4), c(1, 1.6) #over sample
                    ))
 search_criteria <- list(strategy = "RandomDiscrete",
                         max_runtime_secs = 360,
@@ -122,12 +120,11 @@ tuned_drf_perf_valid
 
 gbm_params <- list(learn_rate = seq(0.01, 0.1, 0.01),
                    max_depth = seq(2, 10, 1),
-                   ntrees = seq(10,150,10),
+                   ntrees = seq(10, 150, 10),
                    balance_classes = TRUE,
                    class_sampling_factors = list(
-                        c(1,1),
-                        #c(0.4,1),c(0.6,1),c(0.8,1),#under sample
-                        c(1,1.2),c(1,1.4),c(1,1.6)#over sample
+                        c(1, 1),
+                        c(1, 1.2), c(1, 1.4), c(1, 1.6)#over sample
                    ))
 search_criteria <- list(strategy = "RandomDiscrete",
                         max_runtime_secs = 360)
@@ -164,6 +161,9 @@ tuned_gbm_perf_valid
 ##########
 
 #Look at performance in Test Set
+glm_perf_test <- h2o.performance(model = glm_baseline, newdata = test)
+glm_perf_test
+
 tuned_gbm_perf_test <- h2o.performance(model = tuned_gbm,
                                         newdata = test)
 tuned_gbm_perf_test
@@ -180,47 +180,47 @@ require(ggplot2)
 
 
 # Plot GLM Coefficients
-glm_coef = h2o.coef(glm_baseline)
-glm_coef = data.frame(variable=names(glm_coef), coef=glm_coef)
-glm_coef$sign = as.factor(sign(glm_coef$coef))
+glm_coef <- h2o.coef(glm_baseline)
+glm_coef <- data.frame(variable = names(glm_coef), coef = glm_coef)
+glm_coef$sign <- as.factor(sign(glm_coef$coef))
 
-glm_imp = h2o.varimp(glm_baseline)
-glm_imp = transform(glm_imp, variable = reorder(variable, scaled_importance))
-glm_imp = merge(glm_imp, glm_coef, by = "variable", all.x = TRUE)
+glm_imp <- h2o.varimp(glm_baseline)
+glm_imp <- transform(glm_imp, variable = reorder(variable, scaled_importance))
+glm_imp <- merge(glm_imp, glm_coef, by = "variable", all.x = TRUE)
 
-p = ggplot(glm_imp[order(-glm_imp$scaled_importance),][1:20,], 
-           aes(variable, scaled_importance, fill=sign))
+p <- ggplot(glm_imp[order(-glm_imp$scaled_importance), ][1:20, ],
+           aes(variable, scaled_importance, fill = sign))
 p + geom_bar(stat = "identity") + coord_flip() + theme_minimal()
 
 
 # Plot Variable Importances
-gbm_imp = h2o.varimp(tuned_gbm)
+gbm_imp <- h2o.varimp(tuned_gbm)
 gbm_imp <- transform(gbm_imp, variable = reorder(variable, scaled_importance))
-p = ggplot(gbm_imp, aes(variable, scaled_importance))
+p <- ggplot(gbm_imp, aes(variable, scaled_importance))
 p + geom_bar(stat = "identity") + coord_flip() + theme_minimal()
 
 
 # Accident Severity
-moto_dat_df = tbl_df(as.data.frame(moto_dat))
+moto_dat_df <- tbl_df(as.data.frame(moto_dat))
 
 #MV Involved With
-p = ggplot(moto_dat_df, aes(mviw,fill=severe))
+p <- ggplot(moto_dat_df, aes(mviw, fill = severe))
 p + geom_bar() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 #Violation Category
-p = ggplot(moto_dat_df,aes(pcf_viol_category,fill=severe))
+p <- ggplot(moto_dat_df, aes(pcf_viol_category, fill = severe))
 p + geom_bar() + coord_flip() + theme_minimal()
 
 #Type of Collision
-p = ggplot(moto_dat_df,aes(type_of_collision,fill=severe))
+p <- ggplot(moto_dat_df, aes(type_of_collision, fill = severe))
 p + geom_bar() + coord_flip()
 
 #Highway
-p = ggplot(moto_dat_df,aes(state_hwy_ind,fill=severe))
+p <- ggplot(moto_dat_df, aes(state_hwy_ind, fill = severe))
 p + geom_bar()
 
 #Day and Time
-moto_dat_df$day_of_week = factor(as.character(moto_dat_df$day_of_week),
-                                    levels=c('M','Tu','W','Th','F','Sa','Su'))
-p = ggplot(moto_dat_df,aes(time_of_day,fill=severe))
-p + geom_histogram(binwidth=1) + facet_grid(day_of_week~state_hwy_ind)
+moto_dat_df$day_of_week <- factor(as.character(moto_dat_df$day_of_week),
+                              levels = c("M", "Tu", "W", "Th", "F", "Sa", "Su"))
+p <- ggplot(moto_dat_df, aes(time_of_day, fill = severe))
+p + geom_histogram(binwidth = 1) + facet_grid(day_of_week ~ state_hwy_ind)
--- preprocess.r
+++ preprocess.r
@@ -1,6 +1,6 @@
 require(dplyr)
 require(missForest)
-source('SWITRS_func_and_lookups.r')
+source("SWITRS_func_and_lookups.r")
 
 #SWITRS -- Collision Data
 #http://iswitrs.chp.ca.gov/Reports/jsp/userLogin.jsp
@@ -8,33 +8,33 @@ source('SWITRS_func_and_lookups.r')
 #     Bike, Pedestrian and Motorcycle Data
 #     Dates Jan 2012 - Feb 2017
 #Collision Table
-collision = tbl_df(read.csv('switrs/CollisionRecords.txt',
-                     header=TRUE,
+collision <- tbl_df(read.csv("switrs/CollisionRecords.txt",
+                     header = TRUE,
                      strip.white = TRUE,
-                     na.strings = c("NA","","-")))
+                     na.strings = c("NA", "", "-")))
 
 # #SWITRS -- Collision Party Data
-# party = tbl_df(read.csv('switrs/PartyRecords.txt',
-#                             header=T,
-#                             strip.white = T,
-#                             na.strings = c("NA","","-")))
-#
-# #SWITRS -- Collision Victim Data
-# victim = tbl_df(read.csv('switrs/VictimRecords.txt',
+# party = tbl_df(read.csv("switrs/PartyRecords.txt",
 #                         header=T,
 #                         strip.white = T,
-#                         na.strings = c("NA","","-")))
+#                         na.strings = c("NA", "", "-")))
+#
+# #SWITRS -- Collision Victim Data
+# victim = tbl_df(read.csv("switrs/VictimRecords.txt",
+#                          header=T,
+#                          strip.white = T,
+#                          na.strings = c("NA", "", "-")))
 #
 # #LA Public Works -- Road Surface Condition
 # #https://data.lacity.org/A-Livable-and-Sustainable-City/Road-Surface-Condition-Map/d9rz-k88a
-# pci = tbl_df(read.csv('lacity/road_conditions_2017.csv',
+# pci = tbl_df(read.csv("lacity/road_conditions_2017.csv",
 #                stringsAsFactors = F,
 #                #quote = "\"",
 #                header=T))
 
 #Select Relevant Data
-moto_dat = collision %>%
-  filter(MOTORCYCLE_ACCIDENT == 'Y') %>%
+moto_dat <- collision %>%
+  filter(MOTORCYCLE_ACCIDENT == "Y") %>%
   select(
          #unique id for collision
          CASE_ID,
@@ -62,21 +62,21 @@ moto_dat %>%
 
 
 #Some Quirky Transformations
-moto_dat$collision_date = sapply(moto_dat$COLLISION_DATE,date_format)
-moto_dat$season = as.factor(sapply(moto_dat$COLLISION_DATE,getSeason))
+moto_dat$collision_date <- sapply(moto_dat$COLLISION_DATE, date_format)
+moto_dat$season <- as.factor(sapply(moto_dat$COLLISION_DATE, getSeason))
 
 #Some Less Quirky Transformations
-moto_dat =
+moto_dat <-
   moto_dat %>%
   #Potential DVs
   mutate(
-        mc_killed = as.factor(ifelse(COUNT_MC_KILLED>0, 'Y', 'N')),
-        mc_injured = as.factor(ifelse(COUNT_MC_INJURED>0, 'Y', 'N')),
+        mc_killed = as.factor(ifelse(COUNT_MC_KILLED > 0, "Y", "N")),
+        mc_injured = as.factor(ifelse(COUNT_MC_INJURED > 0, "Y", "N")),
         collision_severity = as.factor(
                   collision_severity_lookup[as.factor(COLLISION_SEVERITY)]),
         severe = as.factor(
-                  ifelse(collision_severity %in% c('Fatal','Injury (Severe)'),
-                         'Y','N'))
+                  ifelse(collision_severity %in% c("Fatal", "Injury (Severe)"),
+                         "Y", "N"))
   ) %>%
   #Calculate Some Time-Related Variables
   mutate(accident_year = ACCIDENT_YEAR,
@@ -85,27 +85,27 @@ moto_dat =
               #check for morning rush hour
               ifelse(
                   (COLLISION_TIME >= 700 & COLLISION_TIME <= 900 #7-9am
-                  & day_of_week %in% c('M','Tu','W','Th','F')
-                  ), 'AM',
+                  & day_of_week %in% c("M", "Tu", "W", "Th", "F")
+                  ), "AM",
               #check for evening rush hour
               ifelse(
                   (COLLISION_TIME >= 1500 & COLLISION_TIME <= 1900 #3-7pm
-                  & day_of_week %in% c('M','Tu','W','Th','F')
-                  ), 'PM','N'))
+                  & day_of_week %in% c("M", "Tu", "W", "Th", "F")
+                  ), "PM", "N"))
               ),
          late_night = as.factor(ifelse(
               (COLLISION_TIME >= 2300 | COLLISION_TIME <= 500), #11pm-5am
-              'Y','N')),
-         hours_since_midnight = floor(COLLISION_TIME/100) #hours
-                                + (COLLISION_TIME %% 100)/60 #minutes (decimal)
+              "Y", "N")),
+         hours_since_midnight = floor(COLLISION_TIME / 100) #hours
+              + (COLLISION_TIME %% 100) / 60 #minutes (decimal)
   ) %>%
   #Transform/Recode Weather Columns [not mutually exclusive]
-  mutate(w_clear  = as.factor(merge_and_recode(WEATHER_1,WEATHER_2,'A')),
-         w_cloudy = as.factor(merge_and_recode(WEATHER_1,WEATHER_2,'B')),
-         w_rainy  = as.factor(merge_and_recode(WEATHER_1,WEATHER_2,'C')),
+  mutate(w_clear  = as.factor(merge_and_recode(WEATHER_1, WEATHER_2, "A")),
+         w_cloudy = as.factor(merge_and_recode(WEATHER_1, WEATHER_2, "B")),
+         w_rainy  = as.factor(merge_and_recode(WEATHER_1, WEATHER_2, "C")),
          #Merge infrequent weather types [snowy, foggy, other, windy]
          w_other  = as.factor(merge_and_recode(
-                      WEATHER_1,WEATHER_2,codes =c('D','E','F','G')))
+                      WEATHER_1, WEATHER_2, codes = c("D", "E", "F", "G")))
   ) %>%
   #Transform Road, Highway Data
   mutate(
@@ -113,8 +113,8 @@ moto_dat =
     state_hwy_ind = as.factor(STATE_HWY_IND),
     road_not_dry = as.factor(
         ifelse(road_surface_lookup[ROAD_SURFACE] %in%
-            c("Slippery (Muddy, Oily, etc.)","Snowy or Icy","Wet"),
-            'Y','N')),
+            c("Slippery (Muddy, Oily, etc.)", "Snowy or Icy", "Wet"),
+            "Y", "N")),
     lighting = as.factor(light_lookup[LIGHTING])
     #control_dev = as.factor(control_dev_lookup[CONTROL_DEVICE])
     #exclude - only 22 non-functioning, 9 obsured, otherwise working/irrelevant
@@ -128,8 +128,8 @@ moto_dat =
               #excludes
               #   flooding (never occurred in data) -- code F
               #   normal condition (not informative) -- code H
-              merge_and_recode(ROAD_COND_1,ROAD_COND_2,
-                               codes =c('A','B','C','D','E','G')))
+              merge_and_recode(ROAD_COND_1, ROAD_COND_2,
+                               codes = c("A", "B", "C", "D", "E", "G")))
   ) %>%
   #Transform Collision Data
   mutate(
@@ -144,7 +144,7 @@ moto_dat =
   #Convert primary and secondary roads to cross streets
   #(later lookup geocodes with: geocode(moto_dat$cross_street)
   mutate(
-    cross_street = paste(PRIMARY_RD,'and',SECONDARY_RD,'Los Angeles')
+    cross_street = paste(PRIMARY_RD, "and", SECONDARY_RD, "Los Angeles")
   ) %>%
   select(
     #unique id for collision
@@ -157,7 +157,7 @@ moto_dat =
     #where
     intersection, state_hwy_ind, #cross_streets,
     #environment/conditions
-    starts_with('w_'), #weather variables
+    starts_with("w_"), #weather variables
     road_not_dry, road_condition_issue, lighting,
     #accident/collision details
     type_of_collision, pcf_viol_category, mviw, alcohol_involved,
@@ -165,13 +165,11 @@ moto_dat =
     #covariates (impact collision-level severity measure)
     pedestrian_accident, bicycle_accident
   )
-
-#View(moto_dat)
 summary(moto_dat)
 
 #Count Rows with NAs in Interesting Features
 collision %>%
-  filter(MOTORCYCLE_ACCIDENT == 'Y') %>%
+  filter(MOTORCYCLE_ACCIDENT == "Y") %>%
   filter(is.na(INTERSECTION) |
            (is.na(WEATHER_1) & is.na(WEATHER_2)) |
            (is.na(STATE_HWY_IND)) |
@@ -187,17 +185,17 @@ collision %>%
 
 #Use Random Forests to Impute Missing Values
 set.seed(1047)
-moto_dat.imp = missForest(as.data.frame(moto_dat[,-1]),variablewise = TRUE)
+moto_dat.imp <- missForest(as.data.frame(moto_dat[, -1]), variablewise = TRUE)
 
 #Out of Bag Error for Imputed Columns
-data.frame('total_NAs' = apply(sapply(moto_dat[,-1],is.na),2,sum),
-           'OOB_Error'=moto_dat.imp$OOBerror) #MSE/PFC
+data.frame("total_NAs" = apply(sapply(moto_dat[, -1], is.na), 2, sum),
+           "OOB_Error" = moto_dat.imp$OOBerror) #MSE/PFC
 
 #Save Dataset Variations
 write.csv(moto_dat,
           file = "processed_data/moto_dat_with_NA.csv",
           row.names = FALSE)
-write.csv(cbind(moto_dat[,1],moto_dat.imp$ximp),
+write.csv(cbind(moto_dat[, 1], moto_dat.imp$ximp),
           file = "processed_data/moto_dat_imputed_NA.csv",
           row.names = FALSE)
 write.csv(na.omit(moto_dat),
